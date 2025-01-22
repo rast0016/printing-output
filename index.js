c186010bcf0185ecfe5d56bce8e963899d587062
rast0016 <rast0016@algonquinlive.com>
2025-01-21 20:45:07 -0500

    js file added

---
+++ index.js
@@ -0,0 +1,28 @@
+// Task 1
+let a = 3, b = 5, c;
+let result = `
+a + b: ${a + b}
+a - b: ${a - b}
+a * b: ${a * b}
+a / b: ${a / b}
+a % b: ${a % b}
+a += b: ${a += b}
+a -= b: ${a -= b}
+a *= b: ${a *= b}
+a /= b: ${a /= b}
+a %= b: ${a %= b}
+a == b: ${a == b}
+a != b: ${a != b}
+a > b: ${a > b}
+a < b: ${a < b}
+!a && !c: ${!a && !c}
+!a || !c: ${!a || !c}
+`;
+console.log(result);
+
+// Task 2
+let first_name = "Preshit"; 
+let last_name = "Rastogi";
+let email = "rast0016@algonquinlive.com"; 
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+console.log(output);
